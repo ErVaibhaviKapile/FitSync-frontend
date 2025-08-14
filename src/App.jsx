d67489d21191752ff460757141ac56e244b41d84
Vaibhavi Kapile <kapilevaibhavi2310@gmail.com>
2025-08-14 13:18:36 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,19 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
-import LoginPage from "./pages/LoginPage";
-import SignupPage from "./pages/SignupPage";
-import DashboardPage from "./pages/DashboardPage";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import SignUp from "./pages/SignupPage";
+import Login from "./pages/LoginPage";
+import Dashboard from "./pages/DashboardPage";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/signup" element={<SignupPage />} />
-        <Route
-          path="/dashboard"
-          element={
-            <DashboardPage />
-          
-          }
-        />
-
+        {/* Redirect root ("/") to SignUp */}
+        <Route path="/" element={<Navigate to="/signup" replace />} />
+        
+        {/* Normal routes */}
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </Router>
   );
